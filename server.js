dba37d622e4758040e0a00770b4e30b2a8740cb3
remyamaryalroy <remyaalroy@gmail.com>
2018-06-23 22:51:56 +0530

    deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
 	   path = require('path');
 const app = express();
-app.use(express.static('./dist/play-with-maps/index.html'));
+app.use(express.static('./dist/play-with-maps'));
 app.get('/*',(req,res)=>{
 	res.sendFile(path.join(__dirname,'/dist/play-with-maps/index.html'));
 	
