5f57319c73239512949be8c996f4e8e4a64c6db0
remyamaryalroy <remyaalroy@gmail.com>
2018-06-25 22:52:19 +0530

    location changes

--- src/app/newmap/newmap.component.ts
+++ src/app/newmap/newmap.component.ts
@@ -17,12 +17,10 @@ export class NewmapComponent implements OnInit {
  lng : Number;
  content ="";
  image_url="";
- res$ = new Observable<any>();
  public show:boolean = false;
- 
  markers: marker[] = [{
-		  lat: 51.673858,
-		  lng: 7.815982,
+		  lat: null,
+		  lng: null,
 		  label: 'A',
 		  draggable: true
 	  }];
@@ -31,17 +29,24 @@ export class NewmapComponent implements OnInit {
   ngOnInit() {
    
   }
+  locationChange(){
+     this.show = false;
+	 this.lat = 0;
+	 this.lng = 0;
+	 this.markers[0].lat = 0;
+	 this.markers[0].lng = 0;
+  }
   
   getLocation(){
-     this.show = true;
     this.mapService.getLocation(this.place).subscribe(
     value =>{
 	   this.lat = Number(value.lat());
-	   this.lng= Number(value.lng());
+	   this.lng = Number(value.lng());
 	   this.markers[0].lat = Number(value.lat());
 	   this.markers[0].lng = Number(value.lng()); 
 	}
    );
+    this.show = true;
   }
   
   mapClicked($event) {
